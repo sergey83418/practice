9752aae8591733d76e2d8e16fc84e1cb85d02bf0
Sergey <sergey83418@gmail.com>
2021-11-19 19:59:15 +0300

    practice 3

--- JS/script.js
+++ JS/script.js
@@ -10,12 +10,37 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt ('Один из последних просмотренных фильмов?', ''),
-b = prompt ('На сколько оцените его?', ''),
-c = prompt ('Один из последних просмотренных фильмов?', ''),
-d = prompt ('На сколько оцените его?', '');
+// const a = prompt ('Один из последних просмотренных фильмов?', ''),
+// b = prompt ('На сколько оцените его?', ''),
+// c = prompt ('Один из последних просмотренных фильмов?', ''),
+// d = prompt ('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt ('Один из последних просмотренных фильмов?', ''),
+//           b = prompt ('На сколько оцените его?', '');
+
+//           if (
+//               a != null  && b != null && a != "" && b != '' && a.length < 50)
+//               {
+//           personalMovieDB.movies[a] = b;
+//           console.log('ok!');
+//               } else {
+//                   console.log('error');
+//                   i--;
+//               }
+// }
+
+if (personalMovieDB.count < 10) {
+    console.log('мало фильмов посмотрели');
+} else if (personalMovieDB.count >= 10 &&  personalMovieDB.count < 30) {
+    console.log('много фильмов посмотрели');
+} else if (personalMovieDB.count >= 30) {
+    console.log('дохуя фильмов посмотрели');
+} else {
+    console.log('error');
+}
 
 console.log (personalMovieDB);
